29728d4d60c45464408b857cb43e00a1a2a586d2
abror yuldashev <yuldashev3787@gmail.com>
2024-07-19 16:41:53 +0500

    1bosqichfull

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 // 1:
 
-let n = 7;
-let m = 5;
+// let n = 7;
+// let m = 5;
 
-let ordaklar = 0;
+// let ordaklar = 0;
 
-function ordaklarSoni() {
-    if(m % 2 === 0) {
-        ordaklar = n + (m / 2);
-        console.log(`O'rdaklar soni: ${ordaklar}`);
-    } else {
-        console.log(-1);
-    }
-}
-ordaklarSoni();
+// function ordaklarSoni() {
+//     if(m % 2 === 0) {
+//         ordaklar = n + (m / 2);
+//         console.log(`O'rdaklar soni: ${ordaklar}`);
+//     } else {
+//         console.log(-1);
+//     }
+// }
+// ordaklarSoni();
 
 // 2: 
 
 
-let randomChipta = 123321;
-let randomChiptaString = String(randomChipta);
-let birinchiUchlik = Number(randomChiptaString[0]) + Number(randomChiptaString[1]) + Number(randomChiptaString[2]);
-let ikkinchiUchlik = Number(randomChiptaString[3]) + Number(randomChiptaString[4]) + Number(randomChiptaString[5]);
+// let randomChipta = 123321;
+// let randomChiptaString = String(randomChipta);
+// let birinchiUchlik = Number(randomChiptaString[0]) + Number(randomChiptaString[1]) + Number(randomChiptaString[2]);
+// let ikkinchiUchlik = Number(randomChiptaString[3]) + Number(randomChiptaString[4]) + Number(randomChiptaString[5]);
 
-function omadliChipta() {
-    if(birinchiUchlik == ikkinchiUchlik) {
-        console.log("YES");
-    }  else {
-        console.log("NO");
-    }
-}
-omadliChipta()
+// function omadliChipta() {
+//     if(birinchiUchlik == ikkinchiUchlik) {
+//         console.log("YES");
+//     }  else {
+//         console.log("NO");
+//     }
+// }
+// omadliChipta()
+
+// 3: 
+
+// let n = 12345;
+// function Yigindi() {
+//     let nString = String(n);
+//     let natija = 0;
+//     for (let i = 0; i < nString.length; i++) {
+//         natija += parseInt(nString[i], 10);
+//     }
+//     console.log(natija);
+// }
+// Yigindi();
+
+
