477f7c5936cf21b6c0f1368001e42ba94ef37f7e
abror yuldashev <yuldashev3787@gmail.com>
2024-07-19 16:08:11 +0500

    birinchibosqich

---
+++ app.js
@@ -0,0 +1,18 @@
+// 1:
+
+// let n = 5;
+// let m = 8;
+
+// let count = 0;
+
+// function ordaklarSoni() {
+//     if(m % 2 === 0) {
+//         count = n + (m / 2);
+//         console.log(`O'rdaklar soni: ${count}`);
+//     } else {
+//         console.log(-1);
+//     }
+// }
+// ordaklarSoni();
+
+// 2: 
